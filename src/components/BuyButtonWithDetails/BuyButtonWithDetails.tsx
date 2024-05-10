02951a324659debb345c252214ee7e469156f93f
Fanny Chien <fannychienn@gmail.com>
2024-05-10 10:21:10 -0300

    chore: Retrieves data in new buy button

--- src/components/BuyButtonWithDetails/BuyButtonWithDetails.tsx
+++ src/components/BuyButtonWithDetails/BuyButtonWithDetails.tsx
@@ -1,5 +1,8 @@
 import { usePDP } from "@faststore/core";
 import { Button as UIButton, ButtonProps } from "@faststore/ui";
+import { priceFormatter } from "../../utils/priceFormatter";
+
+import styles from "./buy-button-with-details.module.scss";
 
 export function BuyButtonWithDetails(props: ButtonProps) {
   // FastStore exposes the data that comes from FastStore API along with FastStore API Extensions inside a provider. Use the usePDP hook to access data from a Product Detail Page (PDP). Refer to: https://developers.vtex.com/docs/guides/faststore/api-extensions-consuming-api-extensions
@@ -7,11 +10,24 @@ export function BuyButtonWithDetails(props: ButtonProps) {
 
   console.log("🚀 ~ PDP context:", context);
 
-  // We will show one of the installments option available if the product has interest free
+  // We will show one of the installments option available if the product has interest free (You should go further and show all available installments options)
+  const installment = context?.data?.product?.availableInstallments[0];
+  const interestFree = installment.installmentInterest === 0 ?? false;
+
   return (
-    <section>
+    <section className={styles.buyButtonWithDetails}>
+      {interestFree && (
+        <span>
+          {`${installment.installmentNumber} interest-free installment(s)`}
+          <br />
+          {`of ${priceFormatter(installment.installmentValue)} with ${
+            installment.installmentPaymentSystemName
+          }`}
+        </span>
+      )}
+
       <UIButton {...props} variant="primary">
-        New Buy Button
+        Buy Button
       </UIButton>
     </section>
   );
